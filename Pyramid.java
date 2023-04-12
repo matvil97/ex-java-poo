2a94d4bd7569e847a70d42943dc2fb48d004baee
matvil97 <84513556+matvil97@users.noreply.github.com>
2023-04-12 11:43:00 +0200

    maJ Pyramid

--- Pyramid.java
+++ Pyramid.java
@@ -1,16 +1,16 @@
 public class Pyramid {
     public static void main(String[] args) {
-        int n = 5; // Nombre de rangées de la pyramide
-        String symbol = "p"; // Symbole utilisé pour construire la pyramide
+        int n = 5; 
+        String symbol = "$"; // => Symbole "$" utilisé sur la pyramide
         
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n - i; j++) {
-                System.out.print(" "); // Affiche des espaces pour décaler la pyramide vers la droite
+                System.out.print(" "); 
             }
             for (int k = 1; k <= i * 2 - 1; k++) {
-                System.out.print(symbol); // Affiche le symbole de la pyramide
+                System.out.print(symbol); 
             }
-            System.out.println(); // Passe à la ligne suivante pour afficher la rangée suivante de la pyramide
+            System.out.println(); // => passage a la ligne suivante
         }
     }
 }
